71f188d2ca1d074ce7d2694a5a623cb2f1cd9ce1
khisapriyam <55364142+khisapriyam@users.noreply.github.com>
2020-12-24 17:25:27 +0100

    final update

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const app = express()
 
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 app.use(cors());
 
 const port = 5000;
@@ -18,27 +18,33 @@ app.get('/', (req, res) => {
     res.send("hello from db it's working fine")
 })
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true});
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  const appointmentCollection = client.db("doctorsPortal").collection("appointments");
+    const appointmentCollection = client.db("doctorsPortal").collection("appointments");
 
-  //making api
-  app.post('/addAppointment', (req, res) => {
-      const appointment = req.body;
-      console.log(appointment);
-      appointmentCollection.insertOne(appointment)
-      .then(result => {
-          res.send(result.insertedCount > 0)
-      })
-  });
+    //making api
+    app.post('/addAppointment', (req, res) => {
+        const appointment = req.body;
+        console.log(appointment);
+        appointmentCollection.insertOne(appointment)
+            .then(result => {
+                res.send(result.insertedCount > 0)
+            })
+    });
+    app.get('/appointments', (req, res) => {
+        appointmentCollection.find({})
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
+    })
 
-  app.post('/appointmentsByDate', (req, res) => {
+    app.post('/appointmentsByDate', (req, res) => {
         const date = req.body;
         console.log(date.date);
-        appointmentCollection.find({date: date.date})
-        .toArray((err, documents) =>{
-            res.send(documents);
-        }) 
+        appointmentCollection.find(filter)
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
     });
 
 });
